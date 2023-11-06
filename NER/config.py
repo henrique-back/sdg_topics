872f3f43781689b5b7968e4d2997eb24a0bded7b
henrique-back <henriqueback72@gmail.com>
2023-11-06 12:20:01 +0100

    Add NER!

--- NER/config.py
+++
@@ -1,9 +0,0 @@
-import get_data.helper as helper
-
-config = helper.read_config()
-
-username = config['GEONAMESSettings']['username']
-
-print("\nDisplaying Access details\n")
-
-print("GEONAMES Username: " + username)
